23f80285f3ef8a6b297c50bdd218905b36ebea32
yasaman <yasamanalz00@gmail.com>
2026-06-29 21:49:59 +0330

    feat: create ProductCard component and integrate it into ChatToppingProduct

---
+++ volta-shop/src/homecomponents/ChartToppingProducts.vue
@@ -0,0 +1,63 @@
+<script setup>
+import ProductCard from './ProductCard.vue'
+import image1 from "../assets/images/rahati.png"
+import image2 from "../assets/images/goldan.png"
+import image3 from "../assets/images/asali.png"
+import image4 from "../assets/images/cheragh.png"
+
+
+
+const products = [
+  {
+    id: 1,
+    title: 'صندلی راحتی نژاد',
+    image: image1,
+    price: 29900000,
+    rate: 4,
+    review: 38,
+  },
+  {
+    id: 2,
+    title: 'گلدان سرامیکی',
+    image: image2,
+    price: 8500000,
+    rate: 5,
+    review: 26,
+  },
+   {
+    id: 3,
+    title: 'میز عسلی چوبی',
+    image: image3,
+    price: 2350000,
+    rate: 3,
+    review: 26,
+  },
+   {
+    id: 4,
+    title: 'چراغ مدرن',
+    image: image4,
+    price: 1530000,
+    rate: 5,
+    review: 26,
+  },
+]
+</script>
+
+<template>
+  <div class="px-4">
+    <h1 class="text-xl font-bold mb-3 mt-7">محصولات پرفروش</h1>
+  <div
+  class="
+    flex gap-4 overflow-x-auto pb-2 snap-x snap-mandatory
+    md:grid md:grid-cols-3 lg:grid-cols-4 md:overflow-visible
+  "
+>
+  <ProductCard
+    v-for="product in products"
+    :key="product.id"
+    :product="product"
+    class="flex-shrink-0 w-40 sm:w-48 md:w-full snap-start"
+  />
+</div>
+  </div>
+</template>
---
+++ volta-shop/src/homecomponents/ProductCard.vue
@@ -0,0 +1,70 @@
+<script setup>
+import { Heart, ShoppingCart, Star } from 'lucide-vue-next'
+
+defineProps({
+  product: {
+    type: Object,
+    required: true,
+  },
+})
+</script>
+
+<template>
+  <div
+    class="group w-32 sm:w-64 lg:w-64 flex-shrink-0 mb-10 rounded-2xl border border-gray-200
+     bg-white overflow-hidden transition-all duration-300 hover:shadow-xl snap-start"
+  >
+    <!-- تصویر -->
+   <div class="relative h-32 sm:h-48 overflow-hidden ">
+  <button
+    class="absolute top-3 left-3 z-10 w-7 h-7 rounded-full bg-white shadow flex items-center justify-center"
+  >
+    <Heart class="w-4 h-4 text-gray-500" />
+  </button>
+
+  <img
+    :src="product.image"
+    :alt="product.title"
+    class="w-full h-full"
+  />
+</div>
+
+    <!-- اطلاعات -->
+    <div class="p-4 flex flex-col justify-center items-center gap-1">
+      <h3
+        class="font-bold text-sm sm:text-lg mb-2 text-gray-800 text-center line-clamp-2"
+      >
+        {{ product.title }}
+      </h3>
+
+      <!-- قیمت -->
+      <p class="text-purple-950 text-sm sm:text-base">
+        {{ product.price.toLocaleString() }}
+        <span class="text-xs sm:text-base font-normal">
+          تومان
+        </span>
+      </p>
+
+      <!-- امتیاز -->
+      <div class="flex items-center gap-1 mb-3">
+        <Star
+          v-for="n in 5"
+          :key="n"
+          class="w-3 h-3 sm:w-4 sm:h-4"
+          :class="n <= product.rate ? 'fill-yellow-400 text-yellow-400' : 'text-gray-300'"
+        />
+
+        <span class="text-xs sm:text-sm text-gray-500 mr-2">
+          ({{ product.review }})
+        </span>
+      </div>
+
+      <!-- دکمه -->
+      <button
+        class="hover:text-purple-800 hover:scale-105 transition flex items-center justify-center"
+      >
+        <ShoppingCart class="w-5 h-5" />
+      </button>
+    </div>
+  </div>
+</template>
