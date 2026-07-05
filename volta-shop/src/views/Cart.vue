77317aaf3e6e599cbde986218d6963ff0e2bd2e3
yasaman <yasamanalz00@gmail.com>
2026-07-05 16:10:20 +0330

    feat:implement Cart component

---
+++ volta-shop/src/views/Cart.vue
@@ -0,0 +1,14 @@
+<script setup>
+import Sidebar from '@/cartcomponents/Sidebar.vue';
+
+</script>
+
+<template>
+  <div class="bg-purple-50">
+    <div class="pt-24 pr-3 sm:pr-5 flex flex-col gap-1">
+      <h1 class="text-2xl font-bold">سبد خرید</h1>
+      <p class="text-gray-700">محصولات مورد نظر خود را بررسی کنید و خریدتان را نهایی کنید.</p>
+    </div>
+    <Sidebar/>
+  </div>
+</template>
