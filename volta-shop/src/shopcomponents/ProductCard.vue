e8d5b8fa53284d37c85cc14577223e7ff1f15605
yasaman <yasamanalz00@gmail.com>
2026-07-08 09:36:26 +0330

    feat(ProductCard): add view details button

--- volta-shop/src/shopcomponents/ProductCard.vue
+++ volta-shop/src/shopcomponents/ProductCard.vue
@@ -5,28 +5,30 @@ import CartIcon from './CartIcon.vue'
 defineProps({
   product: {
     type: Object,
-    required: true
-  }
+    required: true,
+  },
 })
 </script>
 
 <template>
-  <div class="bg-white rounded-2xl overflow-hidden shadow-sm hover:shadow-xl transition-all duration-300 group">
-
+  <div
+    class="bg-white rounded-2xl overflow-hidden shadow-sm hover:shadow-xl transition-all duration-300 group"
+  >
     <div class="relative">
       <img
         :src="product.image"
         :alt="product.title"
-        class="w-full h-56 sm:h-64 object-cover group-hover:scale-105 transition-transform duration-500"
+        class="w-full h-56 sm:h-64 object-fit group-hover:scale-105 transition-transform duration-500"
       />
 
-      <button class="absolute top-3 left-3 w-9 h-9 rounded-full bg-white/90 shadow flex items-center justify-center">
-        <Heart class="w-5 h-5 text-gray-500 hover:text-red-500"/>
+      <button
+        class="absolute top-3 left-3 w-9 h-9 rounded-full bg-white/90 shadow flex items-center justify-center"
+      >
+        <Heart class="w-5 h-5 text-gray-500 hover:text-red-500" />
       </button>
     </div>
 
     <div class="p-4 sm:p-5">
-
       <h4 class="font-medium text-gray-800 text-sm sm:text-base line-clamp-2 min-h-[48px]">
         {{ product.title }}
       </h4>
@@ -38,9 +40,14 @@ defineProps({
         </span>
       </div>
 
-      <CartIcon :product="product"/>
-
+      <CartIcon :product="product" />
+      <RouterLink
+        :to="{ name: 'ProductDetails', params: { id: product.id } }"
+        class="mt-5 block w-full text-center px-8 py-3 rounded-xl text-white
+         bg-gradient-to-br from-gray-900 to-gray-700 hover:scale-105 transition"
+      >
+     مشاهده جزئیات
+      </RouterLink>
     </div>
-
   </div>
 </template>
